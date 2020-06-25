6a26e7a6d07370ae02ce2f4d3982a24a774ddd1d
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-25 23:39:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -50,7 +50,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.collect do |season, holiday|
     holiday.collect do |the_holiday, supply|
-      puts the_holiday if supply.include?("BBQ")
+      the_holiday if supply.include?("BBQ")
     end
   end
 end

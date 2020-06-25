7855e1ab75909503a6eff193aaf726f580013e4c
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-25 23:34:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,7 +44,12 @@ def all_winter_holiday_supplies(holiday_hash)
 end
 
 def all_supplies_in_holidays(holiday_hash)
-  holiday_hash.each do |season, holiday|
+  holiday_hash.collect do |season, holiday|
+    holiday.collect do |supply|
+      if supply == "BBQ"
+        puts holiday
+      end
+    end.flatten
   end
   # iterate through holiday_hash and print items such that your readout resembles:
   # Winter:

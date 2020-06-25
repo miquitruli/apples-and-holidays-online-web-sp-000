5f503b5be41211158133bcf2c9ba194b212b93f6
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-25 23:05:26 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -37,7 +37,6 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-
   holiday_hash.each do |season|
     if season == :winter
       season.each do |holiday,supplies|

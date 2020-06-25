36ffe193882cf2ba238345aea361958c292c55f2
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-25 23:14:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -39,7 +39,7 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   holiday_hash.each do |season, holiday|
-    if season == []:winter
+    if season == :winter
       holiday.each do |supply|
         supply.flatten
       end

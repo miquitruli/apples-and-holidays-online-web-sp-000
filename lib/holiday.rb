87eb3e34f98f98f3e5f2e1add4a80ff48b7e98d7
MICAELLA COLOMBO <micaelc@g.clemson.edu>
2020-06-25 22:52:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -37,11 +37,11 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
+  all_supplies = ("")# return an array of all of the supplies that are used in the winter season
   holiday_hash[:winter].collect do |holiday, supplies|
-    supplies
+    all_supplies<<supplies.flatten
   end
-  supplies.flatten
+  
 end
 
 def all_supplies_in_holidays(holiday_hash)
